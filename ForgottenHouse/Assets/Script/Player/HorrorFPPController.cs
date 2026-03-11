e2eb595fa23547de1727b99e813dd5f349674bb3
ayushmzp72 <ayushmzp72@gmail.com>
2026-03-11 22:51:05 +0530

    2250

--- ForgottenHouse/Assets/Script/Player/HorrorFPPController.cs
+++ ForgottenHouse/Assets/Script/Player/HorrorFPPController.cs
@@ -1,27 +1,68 @@
 using UnityEngine;
-public class HorrorFPPController : MonoBehaviour {
-    public float moveSpeed = 3f;
+
+public class HorrorFPPController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float walkSpeed = 4f;
+    public float runSpeed = 7f;
+    public float jumpForce = 6f;
+    public float gravity = -20f;
+
+    [Header("Mouse")]
     public float mouseSensitivity = 100f;
     public Transform camHolder;
+
     CharacterController cc;
     float xRot = 0f;
     Vector3 velocity;
-    void Start() {
+
+    void Start()
+    {
         cc = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
     }
-    void Update() {
-        float mx = Input.GetAxis("Mouse X")*mouseSensitivity*Time.deltaTime;
-        float my = Input.GetAxis("Mouse Y")*mouseSensitivity*Time.deltaTime;
-        xRot = Mathf.Clamp(xRot - my, -70f, 70f);
+
+    void Update()
+    {
+        MouseLook();
+        Move();
+    }
+
+    void MouseLook()
+    {
+        float mx = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float my = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRot -= my;
+        xRot = Mathf.Clamp(xRot, -70f, 70f);
+
         camHolder.localRotation = Quaternion.Euler(xRot, 0f, 0f);
         transform.Rotate(Vector3.up * mx);
+    }
+
+    void Move()
+    {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        Vector3 move = transform.right*x + transform.forward*z;
-        cc.Move(move * moveSpeed * Time.deltaTime);
-        if (cc.isGrounded && velocity.y < 0) velocity.y = -2f;
-        velocity.y += -9.81f * Time.deltaTime;
+
+        float speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        cc.Move(move * speed * Time.deltaTime);
+
+        if (cc.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && cc.isGrounded)
+        {
+            velocity.y = jumpForce;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
         cc.Move(velocity * Time.deltaTime);
     }
 }
